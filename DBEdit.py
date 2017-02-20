a74a7e3405f7cdabd6e94ef9502c4061d6b436b0
Chris McNutt <cmcnutt1@uncc.edu>
2017-02-20 05:27:30 -0500

    Updated cruise database

---
+++ DBEdit.py
@@ -0,0 +1,275 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from tinydb import TinyDB, Query
+import time
+import os
+from datetime import datetime, timedelta
+from LearnMoreScrape import get_itin_learn_more
+
+
+#Initialize TinyDB for storing cruise info
+def init_db():
+
+    db = TinyDB('cruise.json')
+    return db
+
+def get_dbs():
+    
+    db1 = TinyDB('cruise1.json')
+    db2 = TinyDB('cruise2.json')
+
+    dbList = []
+    dbList.append(db1)
+    dbList.append(db2)
+
+    return dbList
+
+def fix_depart(db_list, list_search, list_entry):
+
+    new_itin = []
+    db = init_db()
+    for item in db_list:
+        index_item = db.get(eid=item)
+        itin_list = index_item['cruise_itin']
+        return_loc = index_item['return_loc']
+        for yo_momma in itin_list:
+            if(list_search in yo_momma):
+                if("Tendered" in yo_momma):
+                    new_itin.append("Tendered at " + list_entry)
+                elif("Docked" in yo_momma):
+                    new_itin.append("Docked at " + list_entry)
+                elif("Depart" in yo_momma):
+                    new_itin.append("Depart from " + list_entry)
+                elif("Disembark" in yo_momma):
+                    new_itin.append("Disembark at " + list_entry)
+            else:
+                new_itin.append(yo_momma)
+        
+        db.update({'return_loc': list_entry}, eids = [item])
+        db.update({'cruise_itin': new_itin}, eids = [item])        
+
+        print("New Itin:")
+        print(db.get(eid=item)['cruise_itin'])
+
+        print("New Return:")
+        print(db.get(eid=item)['return_loc'])
+        new_itin = []
+
+def fix_city(db_list, list_search, list_entry, tag_search, tag_entry):
+
+    new_list = []
+    new_tags = []
+    new_itin = []
+    db = init_db()
+    for item in db_list:
+        index_item = db.get(eid=item)
+        port_list = index_item['port_list']
+        port_tags = index_item['port_tags']
+        itin_list = index_item['cruise_itin']
+        for yo_momma in port_list:
+            if(list_search in yo_momma):
+                new_list.append(list_entry)
+            else:
+                new_list.append(yo_momma)
+
+        for yo_daddy in port_tags:
+            if(tag_search in yo_daddy):
+                addition = tag_entry
+            else:
+                addition = yo_daddy
+            if not (addition in new_tags):
+                new_tags.append(addition)
+
+        for yo_granny in itin_list:
+            if(list_search in yo_granny):
+                if("Tendered" in yo_granny):
+                    new_itin.append("Tendered at " + list_entry)
+                elif("Docked" in yo_granny):
+                    new_itin.append("Docked at " + list_entry)
+                elif("Depart" in yo_granny):
+                    new_itin.append("Depart from " + list_entry)
+                elif("Disembark" in yo_granny):
+                    new_itin.append("Disembark at " + list_entry)
+            else:
+                new_itin.append(yo_granny)
+                    
+        db.update({'port_list': new_list}, eids = [item])
+        db.update({'port_tags': new_tags}, eids = [item])
+        db.update({'cruise_itin': new_itin}, eids = [item])
+        print("\nEntry: " + str(item) + "\n")
+        print("New List:")
+        print(db.get(eid=item)['port_list'])
+        print("New Tags:")
+        print(db.get(eid=item)['port_tags'])
+        print("New Itin:")
+        print(db.get(eid=item)['cruise_itin'])
+        new_list = []
+        new_tags = []
+        new_itin = []
+
+
+def get_individual_entry(query):
+
+    mainDb = init_db()
+
+    i = 1
+
+    incorrect_list = []
+
+    db_len = len(mainDb)
+
+    
+
+    dating_errors = []
+
+    more_days_errors = []
+
+    #Don't think this is a problem. But I'll test for it anyways
+    less_days_errors = []
+
+    pricing_errors = []
+
+    diff_depart = []
+
+    diff_arrive = []
+
+    same_depart = []
+
+    all_ports = []
+
+    target_items = []
+
+     
+    i =1
+
+    while(i < (db_len + 1)):
+
+        entry = mainDb.get(eid=i)
+
+    #********************************************
+    # Get information to write to merged database
+    #********************************************
+
+        cruise_name = entry['cruise_name']
+
+        cruise_len = cruise_name.split()[0]
+
+        departure_loc = entry['departure_loc']
+
+        return_loc = entry['return_loc']
+
+        if (query in return_loc):
+            target_items.append(i)
+
+        ship_name = entry['ship_name']
+
+        port_list = entry['port_list']
+
+        '''for item in port_list:
+            if (query in item):
+                target_items.append(i)'''
+ 
+        port_tags = entry['port_tags']
+
+        
+
+        cruise_sub = entry['cruise_sub']
+
+        available_departure_dates = entry['available_departure_dates']
+        available_return_dates = entry['available_return_dates']
+
+        regular_price = entry['regular_price']
+
+        sale_price = entry['sale_price']
+
+        cruise_itin = entry['cruise_itin']
+
+        i += 1
+
+    #************************
+    # Testing Print Outs
+    #************************
+    '''print("\n\nMore/Less Days Errors:\n**********************\n")
+    
+    if(len(more_days_errors) == 0):
+        print("\nNo difference in expected cruise lengths. Nice")
+
+    else:
+        
+        for err in more_days_errors:
+            print(err)
+
+    print("\n\nNo Dates Available Errors:\n**************************\n")
+
+    if(len(dating_errors) == 0):
+        print("\nNo dating errors. Nice")
+    else:
+        for err in dating_errors:
+            print(err)
+
+    print("\n\nPricing Errors:\n***************\n")
+
+    if(len(pricing_errors) == 0):
+        print("\nNo pricing errors. Nice\n\n")
+
+    else:
+        for err in pricing_errors:
+            print(err)
+        print("\n\n")'''
+
+    '''print("\n\nWhere departure/return location is same:\n*****************************************")
+    for item in same_depart:
+        print(item)
+
+    print("\n\nWhere departure/return locations are different:\n************************************************")
+
+    print("\nDepartures:\n")
+
+    for item in diff_depart:
+        if(item in same_depart):
+            something = 0
+
+        else:
+            print(item)
+
+    print("\nArrivals:\n")
+
+    for item in diff_arrive:
+        if(item in same_depart or item in diff_depart):
+            something = 0
+
+        else:
+            print(item)
+
+    print("\n\nAll unique ports (Excluding arrival/departures):\n*************************************************")
+
+    for item in all_ports:
+        if(item in diff_depart or item in diff_arrive or item in same_depart):
+            something = 0
+        else:
+            print(item)
+    '''
+
+    print(target_items)
+    for item in target_items:
+        print(mainDb.get(eid=item))
+    return target_items        
+        
+
+if __name__ == "__main__":
+    
+    list_query = "Singapore, Singapore"
+    list_entry = "Singapore"
+
+    tag_query = "Tahiti"
+    tag_entry = "Tahiti"
+
+    print(list_query + "\n")
+
+    a_list = get_individual_entry(list_query)
+
+    #fix city (db_list, list_search, list_entry, tag_search, tag_entry)
+    #fix_city(a_list, list_query, list_entry, tag_query, tag_entry)
+
+    fix_depart(a_list, list_query, list_entry)
+    
---
+++ ShipHTML.py
@@ -0,0 +1,110 @@
+
+
+
+
+
+
+
+#*******************
+# Ship HTML Chunks
+#*******************
+
+def get_anthem():
+
+def get_ovation():
+
+def get_quantum():
+
+def get_allure():
+
+def get_harmony():
+
+def get_oasis():
+
+def get_freedom():
+
+def get_independence():
+
+def get_liberty():
+
+def get_adventure():
+
+def get_explorer():
+
+def get_mariner():
+
+def get_navigator():
+
+def get_voyager():
+
+def get_brilliance():
+
+def get_serenade():
+
+def get_enchantment():
+
+def get_grandeur():
+
+def get_legend():
+
+def get_rhapsody():
+
+def get_vision():
+
+def get_majesty():
+
+def get_empress():
+
+def switch_ship(ship):
+
+    ship_name = ship.lower()
+
+    if(ship_name == 'anthem of the seas'):
+        chunk = get_anthem()
+    elif(ship_name == 'ovation of the seas'):
+
+    elif(ship_name == 'quantum of the seas'):
+
+    elif(ship_name == 'allure of the seas'):
+
+    elif(ship_name == 'harmony of the seas'):
+
+    elif(ship_name == 'oasis of the seas'):
+
+    elif(ship_name == 'freedom of the seas'):
+
+    elif(ship_name == 'independence of the seas'):
+
+    elif(ship_name == 'liberty of the seas'):
+
+    elif(ship_name == 'adventure of the seas'):
+
+    elif(ship_name == 'explorer of the seas'):
+
+    elif(ship_name == 'mariner of the seas'):
+
+    elif(ship_name == 'navigator of the seas'):
+
+    elif(ship_name == 'voyager of the seas'):
+
+    elif(ship_name == 'brilliance of the seas'):
+
+    elif(ship_name == 'jewel of the seas'):
+
+    elif(ship_name == 'radiance of the seas'):
+
+    elif(ship_name == 'serenade of the seas'):
+
+    elif(ship_name == 'enchantment of the seas'):
+
+    elif(ship_name == 'grandeur of the seas'):
+
+    elif(ship_name == 'legend of the seas'):
+
+    elif(ship_name == 'rhapsody of the seas'):
+
+    elif(ship_name == 'vision of the seas'):
+
+    elif(ship_name == 'majesty of the seas'):
+
+    elif(ship_name == 'empress of the seas'):
